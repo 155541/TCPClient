fd71b964f39f0ecea9788fe04d81626c7ad8f568
155541 <34254684+155541@users.noreply.github.com>
2018-10-25 15:43:30 +0200

    Update Main.java
    check existsConfigFile done

--- src/com/revolhope/deepdev/tcpclient/main/Main.java
+++ src/com/revolhope/deepdev/tcpclient/main/Main.java
@@ -10,27 +10,75 @@ import javafx.scene.Scene;
 
 
 public class Main extends Application {
+	
+	private static final String pathConfigView = "com/revolhope/deepdev/tcpclient/views/MyFTP_ConfigView.fxml";
+	private static final String pathMainView = "com/revolhope/deepdev/tcpclient/views/MyFTP_MainView.fxml";
+	
+	private static final String pathConfigFile = ".ftp-server_config.txt";
+	
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			
+	public void start(Stage primaryStage) 
+	{
+		try {		
+			URL url;
+			String title = "";
 			FXMLLoader loader = new FXMLLoader();
-			URL url = getClass().getClassLoader().getResource("com/revolhope/deepdev/tcpclient/views/MyFTP_ConfigView.fxml");
+			
+			if (existsConfigFile())
+			{
+				url = getClass().getClassLoader().getResource(pathMainView);
+				title = "LocalShare Client";
+			}
+			else
+			{
+				url = getClass().getClassLoader().getResource(pathConfigView);
+				title = "Configure";
+			}
+			
 	        loader.setLocation(url);
 	        Parent root = (Parent)loader.load();
-	        Scene scene = new Scene(root);
-	        
+	       	Scene scene = new Scene(root);
+	        primaryStage.setTitle(title);
 	        primaryStage.setScene(scene);
-	        primaryStage.setTitle("FTP");
 	        primaryStage.setResizable(false);
 	        primaryStage.show();
-
-		} catch(Exception e) {
+		} 
+		catch(Exception e) 
+		{
 			e.printStackTrace();
 		}
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		launch(args);
+	}
+	
+	public void loadMainView(Stage primaryStage)
+	{
+		try
+		{
+			FXMLLoader loader = new FXMLLoader();
+			URL url = getClass().getClassLoader().getResource(pathMainView);
+			
+			loader.setLocation(url);
+	        Parent root = (Parent)loader.load();
+	        Scene scene = new Scene(root);
+	        
+	        primaryStage.setScene(scene);
+	        primaryStage.setTitle("LocalShare Client");
+	        primaryStage.setResizable(false);
+	        primaryStage.show();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	private boolean existsConfigFile()
+	{
+		File f = new File(pathConfigFile);
+		return f.exists() && !f.isDirectory();
 	}
 }
